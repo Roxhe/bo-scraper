c6d70d2da37a6dd816f261a1148b03b9ba20facc
Roxhe <88476983+Roxhe@users.noreply.github.com>
2021-08-18 14:56:19 +0200

    heading modification

---
+++ bo_scraper.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+url = "http://books.toscrape.com/catalogue/category/books_1/index.html"
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+#book-titles
+class_name_titles = "product_pod"
+titles = soup.find_all("h3")
+for title in titles:
+    print(title.string)
+
+#transformed-titles
+titles_string = []
+for title in titles:
+    titles_string.append(title.string)
+print(titles_string)
+
+heading = ["title"]
+with open('data.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')
+    writer.writerow(heading)
+    for book_titles in zip(titles_string):
+        writer.writerow([book_titles])
