b0ae1a89bf5de9d789517ca65ea5b8a3924ebfec
Roxhe <88476983+Roxhe@users.noreply.github.com>
2021-08-26 09:15:19 +0200

    doctstring test and test get_pages on 'default' category

--- bo_scraper.py
+++ bo_scraper.py
@@ -11,15 +11,32 @@ soup = BeautifulSoup(page.content, 'html.parser')
 sample_page = "http://books.toscrape.com/catalogue/category/books_1/page-"
 
 def get_pages(sample_page, nb):
+    """ Description of what get_pages does.
+    
+    Args :
+        sample_pages (str): base of the url that will be copied to create the list.
+        nb(int) = number of loop executions.
+        
+    Returns :
+        list: list containing your created urls from the base."""
     pages = []
     for i in range(1,nb+1):
         j = sample_page + str(i) + ".html"
         pages.append(j)
     return pages
-pages = get_pages(sample_page,20)
+pages = get_pages(sample_page,50)
 del pages[0]
 pages.insert(0, url)
 print(pages)
+
+#add-categories-pages
+page_default1 = "https://books.toscrape.com/catalogue/category/books/default_15/index"
+sample_page_default = "https://books.toscrape.com/catalogue/category/books/default_15/page-"
+get_pages(sample_page_default, nb=2)
+sample_page_default = get_pages(sample_page_default,8)
+del sample_page_default[0]
+sample_page_default.insert(0, page_default1)
+print(sample_page_default)
 
 #book-titles
 class_name_titles = "product_pod"
