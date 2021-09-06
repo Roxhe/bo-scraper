749b61ab9a9f09c953b17ccf8759dfddbc5ac689
Léo Da Rocha <leo.da.rocha2002@gmail.com>
2021-09-06 14:17:34 +0200

    add functions urlparser & urlbooks

--- scripts.py
+++ scripts.py
@@ -1,6 +1,9 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+
 def get_pages(sample_url, nb):
-    """ Description of what get_pages does.
-    
+    """ 
     Args :
         sample_url (str): base of the url that will be copied to create the list.
         nb(int) = number of loop executions.
@@ -12,3 +15,26 @@ def get_pages(sample_url, nb):
         j = sample_url + str(i) + ".html"
         pages.append(j)
     return pages
+
+def urlparser(url):
+    """
+    Args :
+        url (str): url you want to parse.
+    
+    Returns :
+        soup: html code from the url arg."""
+    req = requests.get(url)
+    soup = bs(req.text, "html.parser")
+    return(soup)
+
+
+def urlbooks(url):
+    """ 
+    Args :
+        url (str): page where are the books whose you want urls.
+        
+    Returns : list contained all the books url of one page"""
+    soup = urlparser(url)
+    return(["/".join(url.split("/")[:-1]) + "/" + x.div.a.get('href') for x in soup.findAll("article", class_ = "product_pod")])
+
+
